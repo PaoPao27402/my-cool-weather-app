3f410be4434b2edf22ffd3543379cacb84d1c687
Paola Ruiloba <75644688+PaoPao27402@users.noreply.github.com>
2021-02-15 19:05:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -107,11 +107,11 @@ let forecast = response.data.list[0];
 console.log(forecast);
 forecastElement.innerHTML = `<div class="col-3">
                 
-<div class="card-body">
-  <h4 class="card-title"id="day-two">Fr</h4>
+<div class="card-body" style="background-color: rgba(239, 248, 254, 0.08);">
+<h4 class="card-title"id="day-one">Thu</h4>
   <h5 class="weather-icon">
-    <i class="fas fa-cloud-sun"></i></h5>
-  <p class="temp-day-two">${Math.round(forecast.main.temp_max)}°C</p>
+  <i class="fas fa-cloud"></i></h5>
+  <p class="temp-day-one">${Math.round(forecast.main.temp_max)}°C</p>
 </div>
 </div>`;
 
